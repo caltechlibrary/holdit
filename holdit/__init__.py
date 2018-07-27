29bba1dc7eb49aa7e5e0c1f67e771e5458c84290
Michael Hucka <mhucka@caltech.edu>
2018-07-26 20:37:31 -0700

    Import more classes

--- holdit/__init__.py
+++ holdit/__init__.py
@@ -17,3 +17,7 @@ file "LICENSE" for more information.
 from .__version__ import __version__, __title__, __description__, __url__
 from .__version__ import __author__, __email__
 from .__version__ import __license__, __copyright__
+
+from .exceptions import UserCancel
+
+from .access import AccessHandlerGUI, AccessHandlerCLI
