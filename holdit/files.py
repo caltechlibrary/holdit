fd6dc7c356fc35fe2d3f001a5765411dd1681bf8
Michael Hucka <mhucka@caltech.edu>
2018-08-03 18:49:57 -0700

    New file of file/path utility functions

---
+++ holdit/files.py
@@ -0,0 +1,71 @@
+'''
+files.py: utilities for working with files.
+'''
+
+import os
+from   os import path
+import sys
+import subprocess
+
+import holdit
+
+
+# Main functions.
+# .............................................................................
+
+def readable(file):
+    '''Returns True if the given 'file' is accessible and readable.'''
+    return os.access(file, os.F_OK | os.R_OK)
+
+
+def module_path():
+    '''Returns the absolute path to our module installation directory.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.
+    return path.abspath(holdit.__path__[0])
+
+
+def holdit_path():
+    '''Returns the path to where Holdit is installed.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.  What we want here is the path to the installation
+    # of the Holdit binary.  I don't know how to get that in a os-independent
+    # way, so I'm punting here.
+    return path.abspath(path.join(module_path(), '..'))
+
+
+def desktop_path():
+    '''Returns the path to the user's desktop directory.'''
+    if sys.platform.startswith('win'):
+        return path.join(path.join(os.environ['USERPROFILE']), 'Desktop')
+    else:
+        return path.join(path.join(path.expanduser('~')), 'Desktop')
+
+
+def rename_if_exists(file, notifier):
+    def rename(f):
+        backup = f + '.bak'
+        notifier.msg('Renaming existing file "{}" to "{}"'.format(f, backup),
+                     'To avoid overwriting the existing file "{}", '
+                     + 'it has been renamed to "{}"'.format(f, backup),
+                     'info')
+        os.rename(f, backup)
+
+    if path.exists(file):
+        rename(file)
+        return
+    full_path = path.join(os.getcwd(), file)
+    if path.exists(full_path):
+        rename(full_path)
+        return
+
+
+def open_file(file):
+    '''Open document with default application in Python.'''
+    # Code originally from https://stackoverflow.com/a/435669/743730
+    if sys.platform.startswith('darwin'):
+        subprocess.call(('open', file))
+    elif os.name == 'nt':
+        os.startfile(file)
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', file))
