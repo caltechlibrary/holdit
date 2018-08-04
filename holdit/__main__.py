434e8dea3df19655491adb502d9548e7b2aa964c
Michael Hucka <mhucka@caltech.edu>
2018-08-03 18:48:40 -0700

    Add options for specifying the template and the output file

--- holdit/__main__.py
+++ holdit/__main__.py
@@ -14,6 +14,7 @@ open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
+from   docxtpl import DocxTemplate
 import os
 import os.path as path
 import plac
@@ -22,9 +23,10 @@ import sys
 import holdit
 from holdit.access import AccessHandlerGUI, AccessHandlerCLI
 from holdit.tind import tind_json, tind_records_from_json
-from holdit.generate import generate_printable_list
+from holdit.generate import generate_printable_doc
 from holdit.messages import color, msg, MessageHandlerGUI, MessageHandlerCLI
 from holdit.network import network_available
+from holdit.files import readable, open_file, rename_if_exists, desktop_path
 from holdit.exceptions import *
 
 
@@ -34,6 +36,8 @@ from holdit.exceptions import *
 @plac.annotations(
     pswd       = ('Caltech access user password',                    'option', 'p'),
     user       = ('Caltech access user name',                        'option', 'u'),
+    output     = ('write the output to the file "O"',                'option', 'o'),
+    template   = ('use file "F" as the TIND record print template',  'option', 't'),
     no_color   = ('do not color-code terminal output (default: do)', 'flag',   'C'),
     no_gui     = ('do not start the GUI interface (default: do)',    'flag',   'G'),
     no_keyring = ('do not use a keyring (default: do)',              'flag',   'K'),
@@ -41,7 +45,8 @@ from holdit.exceptions import *
     version    = ('print version info and exit',                     'flag',   'V'),
 )
 
-def main(user = 'U', pswd = 'P', no_color=False, no_gui=False, no_keyring=False,
+def main(user = 'U', pswd = 'P', output='O', template='F',
+         no_color=False, no_gui=False, no_keyring=False,
          reset=False, version=False):
     '''Generate a list of current hold requests.
 
@@ -63,6 +68,16 @@ add the -R (or /R on Windows) command-line argument to a command.  This
 argument will make Holdit query for the user name and password again even if
 an entry already exists in the keyring or keychain.
 
+By default, Holdit looks for a .docx file named "template.docx" in the
+directory where Holdit is located, and uses that as the template for record
+printing.  If given the -t option (/t on Windows), it will look for the named
+file instead.  If it cannot find a file "template.docx" and is not given an
+explicit template file, Holdit will use a built-in default template file.
+
+Holdit will write the output to a file named "holds_print_list.docx" in the
+user's Desktop directory, unless the -o option (/o on Windows) is given with
+an explicit file path.
+
 If given the -V option (/V on Windows), this program will print version
 information and exit without doing anything else.
 '''
@@ -80,6 +95,10 @@ information and exit without doing anything else.
         user = None
     if pswd == 'P':
         pswd = None
+    if template == 'F':
+        template = None
+    if output == 'O':
+        output = None
 
     # Process the version argument first, because it causes an early exit.
     if version:
@@ -101,6 +120,13 @@ information and exit without doing anything else.
     # still have to resort to non-gui operation if the command line contained
     # options that implicate non-gui actions.
     try:
+        template_path = None
+        if template:
+            template_path = path.abspath(template)
+            if not readable(template_path):
+                template_path = None
+                msg('File "{}" not found or not readable -- using default.'.format(template),
+                    'warn', colorize)
         if use_gui:
             credentials_handler = AccessHandlerGUI(user, pswd)
             message_handler = MessageHandlerGUI()
@@ -109,7 +135,13 @@ information and exit without doing anything else.
             message_handler = MessageHandlerCLI(use_color)
         data = tind_json(credentials_handler, message_handler)
         records = tind_records_from_json(data)
-        generate_printable_list(records)
+        result = generate_printable_doc(records, template_path)
+        if not output:
+            output = os.path.join(desktop_path(), "holds_print_list.docx")
+        rename_if_exists(output, message_handler)
+        result.save(output)
+        if use_gui:
+            open_file(output)
     except (KeyboardInterrupt, UserCancelled):
         if no_gui:
             msg('Quitting.', 'warn', use_color)
